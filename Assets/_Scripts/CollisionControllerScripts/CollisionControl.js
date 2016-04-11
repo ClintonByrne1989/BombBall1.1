4648ff5f94e9897ba26a139beffe038eee87e5bb
clinton byrne <clinton byrne>
2016-04-11 21:47:36 +0800

    Android Test <Fail>

--- Assets/_Scripts/CollisionControllerScripts/CollisionControl.js
+++ Assets/_Scripts/CollisionControllerScripts/CollisionControl.js
@@ -4,14 +4,16 @@ var DevText : GameObject;
 var rb : Rigidbody;
 
 //Public Variables//
-public var player1 : float = 20;
-public var player2 : float = 5;
-public var colheight : float = 2;
+public var Player1 : float = 20;
+public var Player2 : float = 5;
+public var ColHeight : float = 2;
 
-//Static Variables//
+// True / False Variables//
 static var Player1Col = false;
 static var Player2Col = false;
-static var hitstrength : float = 1;
+
+//Static Variables//
+static var HitStrength : float = 1;
 static var Angle : float = 0;
 
 function Start () {
@@ -24,53 +26,42 @@ function FixedUpdate(){
 }
 function Update(){
     //DevText.GetComponent.<Text>().text = hitstrength;
-    hitstrength = Input.acceleration.x;
+    HitStrength = Input.acceleration.x;
     Angle = Input.acceleration.z;
-
-
-
 
    // Reset Angle and hitstrength 
     if(Player2Col == true){
         Angle = 0;
     }
     if(Player2Col == true){
-        hitstrength = 0;
+        HitStrength = 0;
     }
 }
+
+// Detect Collisions
 
 function OnTriggerEnter(obj : Collider) {  
     var name = obj.gameObject.tag;
     
     if (obj.gameObject.tag == "Player") {
         Player1Col = true;
-    		
     }		
 
     if (obj.gameObject.tag == "Player2") {
         Player2Col = true;
     }
 }
+// Collision Functions
 
-    function player1collision (){
+function player1collision (){
         if (Player1Col == true){
-            rb.velocity = Vector3(Angle,colheight,player1 + hitstrength);
+            rb.velocity = Vector3(Angle,ColHeight,Player1 + HitStrength);
             Player1Col = false;
-        }
     }
-    function player2collision(){
+}
+function player2collision(){
         if (Player2Col == true){
-            rb.velocity = Vector3((Random.Range(-5, 5)),colheight,player2);
+            rb.velocity = Vector3((Random.Range(-5, 5)),ColHeight,Player2);
             Player2Col = false;
-        }
     }
-
-
-
-
-
-
-
-
-
-    /*TimerGUI.GetComponent.<Text>().text = "Time: " + CountDownTime;*/
+}
